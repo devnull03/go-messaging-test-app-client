805b1165ee2cb6d13a9bba25890365829bacccf9
devnull03 <56480041+devnull03@users.noreply.github.com>
2022-03-22 14:30:45 +0530

    fix(deploy.sh): incorrect url

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com:devnull03/go-messaging-test-app-client.git master:gh-pages
+git push -f https://github.com/devnull03/go-messaging-test-app-client.git master:gh-pages
 
 cd -
