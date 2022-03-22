78d80f75a6cdd3742fe0eb295507c05335766f81
devnull03 <56480041+devnull03@users.noreply.github.com>
2022-03-22 13:08:49 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import Main from "./components/Main.vue";
+</script>
+
+<template>
+  <Main />
+</template>
+
+<style>
+@import "./assets/base.css";
+
+
+a {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,138 @@
+<template>
+  <main>
+    <div class="input-wrapper">
+      <input
+        type="text"
+        v-model="text"
+        @input="onPress"
+        id="message-box"
+        @keypress.enter="onEnter"
+        placeholder="Message"
+      />
+      <p>
+        {{ typing ? `${userName} is typing . . .` : "&nbsp" }}
+      </p>
+    </div>
+    <div class="message-list">
+      <div class="message" v-for="message in messages">
+        <p>{{ message.username }}</p>
+        {{ message.content }}
+      </div>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import axios from "axios";
+
+export default defineComponent({
+  name: "Main",
+  data() {
+    interface Data {
+      serverAddr: string
+      text: string;
+      messages: { username: string, content: string }[];
+      userName: string;
+      typing: boolean;
+      connection: WebSocket | null;
+    }
+    return <Data>{
+      serverAddr: "ws://localhost:3030/socket",
+      text: "",
+      messages: [],
+      userName: "",
+      typing: false,
+      connection: null,
+    };
+  },
+  methods: {
+    onEnter() {
+      if (!this.text) return;
+      if (!this.connection) return;
+
+      this.connection.send(
+        JSON.stringify({ username: this.userName, content: this.text })
+      );
+
+      //   this.messages.push({ content: this.text });
+      this.text = "";
+      this.typing = false;
+    },
+    onPress() {
+      //   this.typing = this.text.length > 0;
+    },
+    getUserName() {
+      const userName = localStorage.getItem("username");
+      if (userName) {
+        this.userName = userName;
+        return;
+      }
+      this.userName = `anon${Math.floor(Math.random() * 9999) + 1000}`;
+      localStorage.setItem("username", this.userName);
+    },
+  },
+  created() {
+    this.getUserName();
+    console.log("Starting connection to WebSocket Server");
+    this.connection = new WebSocket(this.serverAddr);
+
+    // this.connection.onmessage = function (event) {
+    //   console.log(event);
+    // };
+
+    this.connection.onopen = (event) => {
+      console.log(event);
+      console.log("Successfully connected to the echo websocket server...");
+    };
+    this.connection.onmessage = (event) => {
+      console.log(event);
+      this.messages.push(JSON.parse(JSON.parse(event.data).body));
+    };
+  },
+});
+</script>
+
+<style>
+main {
+  padding: 2rem;
+  padding-bottom: 0.5rem;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column-reverse;
+  row-gap: 2rem;
+}
+
+.message-list {
+  display: flex;
+  flex-direction: column;
+  font-size: large;
+  row-gap: 0.65rem;
+}
+.message p {
+  font-size: x-small;
+  color: rgba(161, 161, 161, 0.64);
+}
+.input-wrapper {
+  display: flex;
+  flex-direction: column;
+  margin: 0%;
+  padding: 0%;
+}
+.input-wrapper p {
+  margin-left: 0.75rem;
+  /* font-size: small; */
+}
+input#message-box {
+  width: 100%;
+  font-size: medium;
+  padding: 1rem;
+  background-color: #313131;
+  border: #313131;
+  border-radius: 0.75rem;
+  color: inherit;
+}
+input:focus-visible {
+  outline: none;
+}
+</style>
